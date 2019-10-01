36a40ed3af82ebe63b0f54e41a574171700885d1
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2019-10-01 16:44:06 +0700

    create HomeController and add react stuff (packs/components/routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homepage#index'
+  get '*path' => redirect('/')
 end
