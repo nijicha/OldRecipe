23c8295543420aa8f1a438b7101a09310c0ef678
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2019-10-02 19:33:16 +0700

    rework assets

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import $ from 'jquery'
-import Popper from 'popper.js'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import App from '../components/App'
+
 import './Index.css'
+import './Index.js'
+
+import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
